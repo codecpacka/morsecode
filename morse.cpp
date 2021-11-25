e7d5cfe5be34c3fb36240d107208a317815caba8
codecpacka <ramprasad.r2600@gmail.com>
2021-11-25 13:51:33 +0530

    author manshi patel

--- morse.cpp
+++ morse.cpp
@@ -3,8 +3,8 @@
 #include "morse.h"
 /**
 *@brief the main function() its ask like an entry points
-*@author codecpacka
-*@date 25 nov 2021
+*@author manshi patel
+*@date 21 nov 2021
 *@details the main function works as an entry point of any programme
 *@param none this function takes no parameters
 *@return  int of value 0;
@@ -15,16 +15,16 @@
 int main()
 {
   char msg[100];
-  // string test[7];
   cout << "enter a code msg" << endl;
   cin.getline(msg, 100);
   string text;
   cout << "encoded text is" << endl;
   text = encoded(msg);
-
   node *root = NULL;
   root = root->insert(data, root, n, 0);
-
+  cout << "\ndecoded text is" << endl;
+  stringtoarr(text, root);
+  /***************testing the construction of tree**********/
   // cout << "viewing in order " << endl;   /// for testing purpose to check whether tree is being
   // root->viewinorder();                   /// creating right or not
   // cout << endl;                          ///
@@ -33,7 +33,5 @@ int main()
   // cout << endl;                          ///
   // cout << "viewing post order" << endl;
   // root->viewpostorder();
-  cout << "\ndecoded text is" << endl;
-  stringtoarr(text, root);
   return 0;
 }
